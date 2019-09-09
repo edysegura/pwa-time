2d5b25edb3ae05a95c4dd6a5210ebdb301573365
Edy Segura <edysegura@gmail.com>
2019-09-09 15:50:13 -0300

    Add cache implementation

---
+++ cache.js
@@ -0,0 +1,45 @@
+const cacheName = 'v1'
+
+const assets = [
+  './',
+  './index.html',
+  './js/libs/moment.min.js',
+  './js/index.js'
+]
+
+async function precache() {
+  const cache = await caches.open(cacheName)
+  return cache.addAll(assets)
+}
+
+async function doCache(request) {
+  console.log('Fetching and adding the response data into cache')
+
+  const cache = await caches.open(cacheName)
+  const requestCloned = request.clone()
+  const response = await fetch(requestCloned)
+
+  cache.put(request, response.clone())
+
+  return response
+}
+
+async function fetchFromCacheWhenAvailable(request) {
+  const cache = await caches.open(cacheName)
+  const match = await cache.match(request)
+
+  if (match) {
+    console.log('Returning from cache')
+    return match
+  }
+
+  return doCache(request)
+}
+
+self.addEventListener('install', event => {
+  event.waitUntil(precache().then(() => self.skipWaiting()))
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetchFromCacheWhenAvailable(event.request))
+})
