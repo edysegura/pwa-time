18fa421f290212da831dabaac105c2926d576409
Edy Segura <edysegura@gmail.com>
2019-09-10 16:55:14 -0300

    Use class to organize app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
-const togetherElement = document.getElementById('together')
-const marriedElement = document.getElementById('married')
-const sonAgeElement = document.getElementById('son-age')
-const removeSufix = true
+class App {
 
-togetherElement.textContent = moment('20040322').fromNow(removeSufix)
-marriedElement.textContent = moment('20080612').fromNow(removeSufix)
-sonAgeElement.textContent = moment('20180311').fromNow(removeSufix)
+  constructor() {
+    this.calculate()
+  }
+
+  calculate() {
+    const togetherElement = document.getElementById('together')
+    const marriedElement = document.getElementById('married')
+    const sonAgeElement = document.getElementById('son-age')
+    const removeSufix = true
+
+    togetherElement.textContent = moment('20040322').fromNow(removeSufix)
+    marriedElement.textContent = moment('20080612').fromNow(removeSufix)
+    sonAgeElement.textContent = moment('20180311').fromNow(removeSufix)
+
+  }
+}
+
+new App()
